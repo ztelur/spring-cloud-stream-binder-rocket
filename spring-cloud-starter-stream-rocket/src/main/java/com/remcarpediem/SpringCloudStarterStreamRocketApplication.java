3ae2fc08461559321d335e8c0aa809c5e8c9168b
ztelur <ztelur@gmail.com>
2017-10-02 23:02:45 +0800

    first commit

---
+++ spring-cloud-starter-stream-rocket/src/main/java/com/remcarpediem/SpringCloudStarterStreamRocketApplication.java
@@ -0,0 +1,12 @@
+package com.remcarpediem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStarterStreamRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStarterStreamRocketApplication.class, args);
+	}
+}
---
+++ spring-cloud-starter-stream-rocket/src/test/java/com/remcarpediem/SpringCloudStarterStreamRocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.remcarpediem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStarterStreamRocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
